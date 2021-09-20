6f0c51fd275a0af11e469b93a5e61f5995ba4545
MSI <obviouslynotfakemail@gmail.com>
2021-09-20 22:55:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CurrencyConvertor
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+     
+      
+      Console.WriteLine("Введите сумму");
+      
+      string SummaConv = Console.ReadLine();
+     
+      Console.WriteLine("Валюту, из которой будет произведена конвертация");
+     
+      Console.WriteLine("Пример :долларов, евро");
+
+      string Valuta = Console.ReadLine();
+
+
+      double Itog = double.Parse(SummaConv);
+
+
+      switch (Valuta)
+      {
+        case "доллары":
+          Itog = Itog * 70;
+          break;
+
+        case "евро":
+          Itog = Itog * 80;
+          break;
+
+      }
+
+      Console.WriteLine(Itog);
+
+
+
+
+
+
+          
+
+      Console.ReadKey();
+    }
+  }
+}
